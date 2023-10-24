79af226c807393715f7e33d0c55aa5a4581f4bbd
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2023-10-24 10:45:36 +0700

    test: create manual testing .py for p2p basic communication
    experience.py file contain main peer to connect to, this file should be run first for the other 2 peer to be able to connect

---
+++ experience.py
@@ -0,0 +1,20 @@
+import socket
+import threading
+
+import client.peer as client
+
+
+hostname = socket.gethostname()
+host = socket.gethostbyname(hostname)
+print(f"{host}")
+port = 5252
+newClient = client.Peer(host, port)
+newClient.start()
+
+while True:
+    msg = input(">")
+    newClient.sendMsg(msg)
+    if msg == "stop":
+        break
+
+newClient.socket.close()
---
+++ experienceSent.py
@@ -0,0 +1,15 @@
+import socket
+import threading
+
+import client.peer as client
+
+hostname = socket.gethostname()
+host = socket.gethostbyname(hostname)
+print(f"{host}")
+port = 12542        
+newClient = client.Peer(host, port)
+
+newClient.ConnectWithPeer(hostname, 5252)
+
+while True:
+    newClient.receiveMsg()
