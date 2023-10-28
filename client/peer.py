ac85b824d1c10bea159b02954b3985677bee17d4
Khoa LTN <vietnamkhoale@gmail.com>
2023-10-28 21:13:16 +0700

    refactor: private attributes and magic number 5

--- client/peer.py
+++ client/peer.py
@@ -1,57 +1,71 @@
 import socket
 import threading
 
+"""
+For socket.listen(MAX_NONACCEPTED_CONN).
+
+socket.listen([backlog]) backlog here controls how many 
+non-accept()-ed connections are allowed to be outstanding.
+Should be set to 5
+
+https://stackoverflow.com/questions/2444459/python-sock-listen
+"""
+MAX_NONACCEPTED_CONN = 5
+
+
 class Peer:
     def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connections = []
+        self._host = host
+        self._port = port
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._connections = []
         print(f"Successfully create peer {host} : {port}")
-        #self.start()
-        
+
     def ConnectWithPeer(self, otherPeerHost, otherPeerPort):
         try:
-            connectionEdge = self.socket.connect((otherPeerHost,otherPeerPort))
-            self.connections.append(connectionEdge)
+            connectionEdge = self._socket.connect((otherPeerHost, otherPeerPort))
+            self._connections.append(connectionEdge)
             print(f"Successfully connected with {otherPeerHost} : {otherPeerPort}")
             print(f"Connection Edge = {connectionEdge}")
         except socket.error as connectionError:
-            print(f"Error occur trying to connect with {otherPeerHost} : {otherPeerPort}")
+            print(
+                f"Error occur trying to connect with {otherPeerHost} : {otherPeerPort}"
+            )
             print(f"Error code: {connectionError}")
-    
+
     def listeningToConnect(self):
-        self.socket.bind((self.host, self.port))
-        self.socket.listen(5)
-        
-        #*listen([backlog]) backlog here controls how many non-accept()-ed connections are allowed to be outstanding.
-        #* should be set to 5
-        #* more detail on stack overflow please read: https://stackoverflow.com/questions/2444459/python-sock-listen
-        
-        print(f"Listening for new connection to {self.host} : {self.port}")
+        self._socket.bind((self._host, self._port))
+        self._socket.listen(MAX_NONACCEPTED_CONN)
+
+        print(f"Listening for new connection to {self._host} : {self._port}")
         while True:
-            connectionEdge, otherPeerAddress = self.socket.accept()
-            self.connections.append(connectionEdge)
+            connectionEdge, otherPeerAddress = self._socket.accept()
+            self._connections.append(connectionEdge)
             (otherPeerHost, otherPeerPort) = socket.getnameinfo(otherPeerAddress, True)
             otherPeerPort = int(otherPeerPort)
             print(f"connection : {connectionEdge}")
             print(f"Allow connection from {otherPeerAddress}")
             #
-            
-    
+
     def sendMsg(self, data):
-        for connection in self.connections:
+        for connection in self._connections:
             try:
                 connection.sendall(data.encode())
             except socket.error as communicateError:
                 print(f"False to communicate. Error: {communicateError}")
+
     def receiveMsg(self):
-        (msg ,  otherPeerAddress) = self.socket.recvfrom(1024)
+        (msg, otherPeerAddress) = self._socket.recvfrom(1024)
         # (otherPeerHost, otherPeerPort) = socket.getnameinfo(otherPeerAddress, True)
         print(f"Receive message from {otherPeerAddress} : {msg}")
+
     def start(self):
-        listenThread = threading.Thread(target = self.listeningToConnect)
+        listenThread = threading.Thread(target=self.listeningToConnect)
         listenThread.start()
-    
-    
-    
+        # question?  Only two threads here? Not very generic!
+
+    def publish(lname: str, fname: str):
+        pass
+
+    def fetch(fname: str):
+        pass
