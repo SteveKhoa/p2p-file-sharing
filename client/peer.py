484c01a111e5c91b5447ea503867967e208c6042
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2023-10-30 10:11:03 +0700

    refactor: Change all variable name pattern to fit with pep8

--- client/peer.py
+++ client/peer.py
@@ -3,10 +3,10 @@ import threading
 
 class Peer:
     def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connections = []
+        self._host = host
+        self._port = port
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._connections = []
         print(f"Successfully create peer {host} : {port}")
         #self.start()
         
@@ -15,8 +15,8 @@ class Peer:
         Connect this peer with another peer with given host and port
         '''
         try:
-            connection_edge = self.socket.connect((other_peer_host,other_peer_port))
-            self.connections.append(connection_edge)
+            connection_edge = self._socket.connect((other_peer_host,other_peer_port))
+            self._connections.append(connection_edge)
             print(f"Successfully connected with {other_peer_host} : {other_peer_port}")
             print(f"Connection Edge = {connection_edge}")
         except socket.error as connection_error:
@@ -27,17 +27,17 @@ class Peer:
         '''
         Bind this peer socket to start listening to incoming connection continuously 
         '''
-        self.socket.bind((self.host, self.port))
-        self.socket.listen(5)
+        self._socket.bind((self._host, self._port))
+        self._socket.listen(5)
         
         #*listen([backlog]) backlog here controls how many non-accept()-ed connections are allowed to be outstanding.
         #* should be set to 5
         #* more detail on stack overflow please read: https://stackoverflow.com/questions/2444459/python-sock-listen
         
-        print(f"Listening for new connection to {self.host} : {self.port}")
+        print(f"Listening for new connection to {self._host} : {self._port}")
         while True:
-            connection_edge, other_peer_address = self.socket.accept()
-            self.connections.append(connection_edge)
+            connection_edge, other_peer_address = self._socket.accept()
+            self._connections.append(connection_edge)
             (other_peer_host, other_peer_port) = socket.getnameinfo(other_peer_address, True)
             other_peer_port = int(other_peer_port)
             print(f"connection : {connection_edge}")
@@ -47,7 +47,7 @@ class Peer:
         '''
         Send data to all connections in connections list
         '''
-        for connection in self.connections:
+        for connection in self._connections:
             try:
                 connection.sendall(data.encode())
             except socket.error as communicate_error:
@@ -57,9 +57,9 @@ class Peer:
         '''
         Receive msg from sender
         '''
-        (msg ,  other_peer_address) = self.socket.recvfrom(1024)
+        (msg ,  other_peer_address) = self._socket.recvfrom(1024)
         if msg == "stop":
-            self.socket.close();
+            self._socket.close();
             return
         # (other_peer_host, other_peer_port) = socket.getnameinfo(other_peer_address, True)
         print(f"Receive message from {other_peer_address} : {msg}")
@@ -68,16 +68,16 @@ class Peer:
         '''
         Create a new thread to start listening to others listen thread parallel with sending msg
         '''
-        self.listenThread = threading.Thread(target = self.listening_to_connect)
-        self.listenThread.start()
+        self._listen_thread = threading.Thread(target = self.listening_to_connect)
+        self._listen_thread.start()
         
     def stop_listening_for_connection(self):
         '''
         Stop listening for another connection, clear all connections
         '''
-        print(self.listenThread)
-        self.listenThread.join()
+        print(self._listen_thread)
+        self._listen_thread.join()
         print("Clear all connection")
-        self.connections.clear()
+        self._connections.clear()
         
     
