e62f5370a4dfed08424b499b1927553f669ff043
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-11-03 08:38:44 +0700

    refactor: separate function in _handle_client

--- server/server.py
+++ server/server.py
@@ -7,7 +7,7 @@ SERVER_PORT = 12345
 
 class Server:
     def __init__(self):
-        self._peers = {}  # A dictionary to store peer information (address, port, available files)
+        self._peers = {}  # A dictionary to store peer information [(address, port)] = list of available files
         self._lock = threading.Lock()
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind((SERVER_HOST, SERVER_PORT))
@@ -26,7 +26,7 @@ class Server:
 
         # Seperate request and data from the package
         package = client_socket.recv(1024).decode("utf-8").strip()
-        request, data = pack.split("/")
+        request, data = package.split("/")
         #print(pack)
 
         try:
@@ -35,45 +35,22 @@ class Server:
                 
                 host, port, files = data.split(":")
                 available_files = files.split(",")
-                with self._lock:
-                    if (host, int(port)) in self._peers:
-                        self._peers[(host, int(port))].extend(available_files)
-                    else:
-                        self._peers[(host, int(port))] = available_files
-
+                self._post(host, port, available_files)
+                
             elif(request == "_request_end"):
             # Process a REQUEST_END request from a peer to remove a file from its list
             # or remove itself from the server
                 host, port, removed_file = data.split(":")
-                if not removed_file:
-                    with self._lock:
-                        del self._peer[(host, int(port))]
-                else:
-                    with self._lock:
-                        if (host, int(port)) in self._peers:
-                            # Remove the specified file from the peer's list of available files
-                            if removed_file in self._peers[(host, int(port))]:
-                                self._peers[(host, int(port))].remove(removed_file)
+                self._request_end(host, port, removed_file)
 
 
             elif request == "_get_peer":
             # Process a GET_PEER request from a peer looking for a file
-                filename = data
-                matching_peers = []
+            # Return a list of peers with the requested file
+            # in the form of a string (comma seperated between each)
+                host, port, files = data.split(":")
+                self._get_peer(client_socket, files)
 
-                with self._lock:
-                    for peer, files in self._peers.items():
-                        if filename in files:
-                            matching_peers.append(f"{peer[0]}:{peer[1]}")
-
-                if matching_peers:
-                    # Send a list of peers with the requested file to the client
-                    # in the form of a string (comma seperated between each)
-                    # Should looks like this: 127.0.0.1:1234,222.222.3.4:3456
-                    client_socket.send(",".join(matching_peers).encode("utf-8"))
-                else:
-                    # No peer with the requested file
-                    client_socket.send("No peers with the requested file.".encode("utf-8"))
             else: 
                 print("Unknown request")
         
@@ -82,6 +59,54 @@ class Server:
 
         finally:
             client_socket.close()
+
+
+    def _post(self, host, port, available_files):
+        # Add the peer to the self._peers dictionary
+        # with the list of available files
+        # or update the list of available files if the peer is already in the dictionary
+
+        with self._lock:
+            if (host, int(port)) in self._peers:
+                self._peers[(host, int(port))].extend(available_files)
+            else:
+                self._peers[(host, int(port))] = available_files
+
+
+    def _request_end(self, host, port, removed_file):
+        # Remove the specified file from the peer's list of available files
+        # or remove the peer from the self._peers dictionary if no file is specified
+
+        with self._lock:
+            if not removed_file:
+                del self._peer[(host, int(port))]
+            else:
+                if (host, int(port)) in self._peers:
+                    if removed_file in self._peers[(host, int(port))]:
+                        self._peers[(host, int(port))].remove(removed_file)
+            
+
+    def _get_peer(self, client_socket, filename):
+        # Return a list of peers with the requested file
+        # in the form of a string (comma seperated between each)
+        # Should looks like this:
+
+        matching_peers = []
+
+        with self._lock:
+            for peer, files in self._peers.items():
+                if filename in files:
+                    matching_peers.append(f"{peer[0]}:{peer[1]}")
+
+        if matching_peers:
+            # Send a list of peers with the requested file to the client
+            # in the form of a string (comma seperated between each)
+            # Should looks like this: 127.0.0.1:1234,222.222.3.4:3456
+            client_socket.send(",".join(matching_peers).encode("utf-8"))
+        else:
+            # No peer with the requested file
+            client_socket.send("No peers with the requested file.".encode("utf-8"))
+
 
     def start(self):
         print("listening...")
